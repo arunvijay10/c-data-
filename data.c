d9fcd2294234f55f5f7da531f6a8ef14a026380f
Parrot Live user <user@localhost.localdomain>
2020-08-26 12:29:36 +0000

    array.c

--- data.c
+++ data.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int  main()
 {
-printf("Data In c ");
+printf("Data In c \n");
+int data[10] ,i ;
+printf("Enter 10 number\n");
+for(i=0;i<10;i++)
+{
+scanf("%d",&data[i]);
+}
+for(i=0;i<10;i++)
+{
+printf("This is the number%d",data[i]);
+}
 return 0;
 }
